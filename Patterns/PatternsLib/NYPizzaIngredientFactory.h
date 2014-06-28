283d98ffea11343e0d2d2c095cb23984ec30bd49
Chris Drew <chris@cdrew.co.uk>
2014-06-28 07:17:15 +0100

    Work in progress.

---
+++ Patterns/PatternsLib/NYPizzaIngredientFactory.h
@@ -0,0 +1,10 @@
+#pragma once
+#include "PatternsLib/PizzaIngredientFactory.h"
+
+namespace Patterns {
+
+  class NYPizzaIngredientFactory : public PizzaIngredientFactory {
+
+  };
+
+}  // namespace Patterns
---
+++ Patterns/PatternsLib/PizzaIngredientFactory.h
@@ -0,0 +1,21 @@
+#pragma once
+#include <memory>
+#include <vector>
+
+namespace Patterns {
+  class Dough;
+  class Sauce;
+  class Cheese;
+  class Pepperoni;
+  class Veggie;
+
+  class PizzaIngredientFactory {
+    typedef std::vector<std::unique_ptr<Veggie>> Veggies;
+    std::unique_ptr<Dough> createDough();
+    std::unique_ptr<Sauce> createSauce();
+    std::unique_ptr<Cheese> createCheese();
+    Veggies createVeggies();
+    std::unique_ptr<Pepperoni> createPepperoni();
+  };
+
+}  // namespace Patterns
