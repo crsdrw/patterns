63a8dacdb59629c85bee492f06c30b43d5d2649d
Chris Drew <chris@cdrew.co.uk>
2014-08-06 05:18:58 +0100

    Add C# project for Lazy Singleton.

---
+++ PatternsCSharp/PatternsCSharp/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace PatternsCSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (Singleton.IsInstanceCreated)
+                Console.WriteLine("You should be lazy!");
+            var s1 = Singleton.Instance;
+            var s2 = Singleton.Instance;
+            if (s1 == s2)
+                Console.WriteLine("There can be only one!");
+            if (Singleton.IsInstanceCreated)
+                Console.WriteLine("Instance created.");
+            //var s3 = new Singleton(); // doesn't compile, good.
+        }
+    }
+}
---
+++ PatternsCSharp/PatternsCSharp/Singleton.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace PatternsCSharp
+{
+    public sealed class Singleton
+    {
+        private static readonly Lazy<Singleton> lazy =
+            new Lazy<Singleton>(() => new Singleton());
+
+        public static Singleton Instance { get { return lazy.Value; } }
+        public static bool IsInstanceCreated { get { return lazy.IsValueCreated; } }  // optional
+
+        private Singleton()
+        {
+        }
+    }
+}
